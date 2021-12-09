c4f5473918efa3a1d279a83ce29cceb3bbdf4a43
Jordi9047 <95035711+Jordi9047@users.noreply.github.com>
2021-12-09 17:20:20 +0000

    Add files via upload

--- Ex1.java
+++ Ex1.java
@@ -5,11 +5,9 @@ import java.util.*;
 import general.*;
 
 public class Ex1 {
-	
-	static ArrayList<String> checkout= new ArrayList<String>();//Overall strings (and maybe prices) for printing the recipe
-	
+		
 	//Final Reciept printing array lists
-	static ArrayList<String> hotFoodBasket= new ArrayList<String>();//Basket from hot food section to be added onto checkout for printing
+	static ArrayList<String> basketItems= new ArrayList<String>();//Basket from hot food section to be added onto basketItems for printing
 	static ArrayList<String> notes= new ArrayList<String>();
 	static ArrayList<Double> priceBasket= new ArrayList<Double>();
 	static ArrayList<Integer> qty= new ArrayList<Integer>();
@@ -24,11 +22,14 @@ public class Ex1 {
 	static ArrayList<Double> sidesPrice= new ArrayList<Double>();
 	
 	static int option = 0;
-	
+	static int quantity = 0;
 	static char yesNo = ' ';
+	static String note = "";
 	
+	static double total = 0.0;
 	public static void main(String[] args)
 	{
+		
 		arraysAdd();
 		
 		mainMenu();
@@ -62,11 +63,13 @@ public class Ex1 {
 		hotFoodMeal.add("+1 side £4.50");
 		hotFoodMeal.add("+ Crusty roll £2.75 ");
 		hotFoodMeal.add("+ Crusty roll £2.75 ");
-		hotFoodMeal.add("*Nothing available* ");
+		hotFoodMeal.add("*Nothing available* ");//Needs to be made into a meal, and case 4 needs to be cut and pasted to case 5
 		hotFoodMeal.add("*Nothing available* ");
 		hotFoodMeal.add("*Nothing available* ");
 		hotFoodMeal.add("*Nothing available* ");
 		hotFoodMeal.add("+1 side £3.50 or +2 sides £4.00");
+		
+		//hotFoodMeal.add(9,"*Nothing available* ");
 		
 		mealPrice.add(5.00);
 		mealPrice.add(4.50);
@@ -85,6 +88,8 @@ public class Ex1 {
 		sides.add("Curry Dip");
 		sides.add("Gravy Dip");
 		
+		//hotFoodArray.addAll(sides);
+		
 		sidesPrice.add(0.60);
 		sidesPrice.add(0.40);
 		sidesPrice.add(1.00);
@@ -94,11 +99,13 @@ public class Ex1 {
 		sidesPrice.add(0.60);
 		sidesPrice.add(0.60);
 		sidesPrice.add(0.60);
+		
+		//hotFoodPrice.addAll(sidesPrice);
 	}
 	
 	static void hotFood ()
 	{
-		int option = 0;
+		option = 0;
 
 		System.out.printf("\n%-20s|%-20s|%-20s", "HOT FOOD", "PRICE", "DEALS");
 		System.out.print("\n----------------------------------------------------------------");
@@ -129,14 +136,17 @@ public class Ex1 {
 			
 			if (yesNo == 'N')
 				{
-					hotFoodBasket.add(hotFoodArray.get(foodItem));
+					basketItems.add(hotFoodArray.get(foodItem));
 					priceBasket.add(hotFoodPrice.get(foodItem));
-					System.out.println("\nAdded to basket");
+					addNotes();
+					System.out.println("\n" + (hotFoodArray.get(foodItem)) + "\nAdded to basket");
+					quantity = quantity();
+					qtyTotal(quantity, hotFoodPrice.get(foodItem));
 					goHome();
 				}
 			else
 			{
-				hotFoodBasket.add(hotFoodArray.get(foodItem)+ " Meal");
+				basketItems.add(hotFoodArray.get(foodItem)+ " Meal");
 				priceBasket.add(mealPrice.get(foodItem));
 				Ex1.sidesMenuDeal();
 			}
@@ -148,10 +158,12 @@ public class Ex1 {
 		case 8:
 			foodItem = foodItem-1;
 			System.out.printf("\nYou've ordered: \n%10s (£%.2f)", hotFoodArray.get(foodItem),hotFoodPrice.get(foodItem));
-			hotFoodBasket.add(hotFoodArray.get(foodItem));
+			basketItems.add(hotFoodArray.get(foodItem));
 			priceBasket.add(hotFoodPrice.get(foodItem));
-			quantity();
-			System.out.println("\nAdded to basket");
+			addNotes();
+			quantity = quantity();
+			qtyTotal(quantity, hotFoodPrice.get(foodItem));
+			System.out.println("\n" + (hotFoodArray.get(foodItem)) + "\nAdded to basket");
 			goHome();
 			break;
 			
@@ -166,14 +178,14 @@ public class Ex1 {
 								
 			if (yesNo == 'N')
 				{
-					hotFoodBasket.add(hotFoodArray.get(foodItem));
+					basketItems.add(hotFoodArray.get(foodItem));
 					priceBasket.add(hotFoodPrice.get(foodItem));
 					System.out.printf("\nAdded to basket");
-					System.out.printf("\nAdded" + hotFoodBasket.toString());
+					System.out.printf("\nAdded" + basketItems.toString());
 				}
 					else
 					{
-						hotFoodBasket.add(hotFoodArray.get(foodItem));
+						basketItems.add(hotFoodArray.get(foodItem));
 						option = Ex1.printFoodOption(option);
 						if (option == 1)
 						{
@@ -192,39 +204,85 @@ public class Ex1 {
 		
 	static void reciept ()
 	{
-		System.out.printf("\n%-25s|%-20s|%-15s|%-15s|%-15s", "Item", "Notes", "Price", "Qty", "Subtotal");
-		System.out.print("\n-------------------------------------------------------------------------------");
-		for (int i = 0; i < hotFoodBasket.size(); i++) 
+		System.out.printf("\n%-25s|%-40s|%-15s|%-15s|%-15s", "Item", "Notes", "Price", "Qty", "Subtotal");
+		System.out.print("\n************************************************************************************************************");
+		for (int i = 0; i < basketItems.size(); i++) 
 		{
-			System.out.printf("\n%-25s|%-20s|£%-14.2f|%-15s|£%-15.2f", hotFoodBasket.get(i), notes.get(i), priceBasket.get(i), qty.get(i), subTotal.get(i));
+			System.out.printf("\n%-25s|%-40s|£%-14.2f|%-15s|£%-15.2f", basketItems.get(i), notes.get(i), priceBasket.get(i), qty.get(i), subTotal.get(i));
 			//System.out.printf("\n----------------------------------------------");
 		}
-		//System.out.print(hotFoodBasket.toString());
+		System.out.print("\n************************************************************************************************************");
+		System.out.printf("\n\nTotal Price £%.2f " ,totalPrice(subTotal));
+	}
+	
+		public static double totalPrice(List<Double> list) 
+		{
+		    int sum = 0;
+		    for (double i: list) {
+		        sum += i;
+		    }
+		    return sum;
+		}
+	
+	static void sidesMenu() 
+	{
+		{
+			System.out.printf("\n%-24s|%-20s", "SIDES", "PRICE");
+			
+			System.out.print("\n----------------------------------");
+			
+				for (int i = 0; i < sides.size(); i++) 
+				{
+					System.out.printf("\n\n%-1s)%-22s|£%-19.2f", i+1, sides.get(i), sidesPrice.get(i));//Only our sides will be printed out here,even without the price. The meal has a set price and they can choose whatever side the customer wants
+					//System.out.printf("\n----------------------------------------------");
+				}
+				
+			int sideOption = MyVal.validIntRange("\n\nPlease choose an option above", 1, sides.size());
+			
+			System.out.println("\n" + sides.get(sideOption-1) + "\nAdded as a side.");
+			
+			int quantity = quantity();
+			
+			qtyTotal(quantity, mealPrice.get(option-1));
+			
+			notes.add("+ " + sides.get(option-1) + " side");
+			
+			yesNo = MyVal.validYN("\nReturn to hot food selection (Y), or home (N)?");
+			
+			if (yesNo == 'N')
+			{
+				mainMenu();
+			}
+				else if (yesNo == 'Y')
+				{
+					hotFood();
+				}
+		}
 	}
 	
 	static void sidesMenuDeal() 
 	{
-		System.out.printf("\n%-24s|%-20s", "SIDES", "PRICE");
+		System.out.printf("\n%-24s", "SIDES");
 		
-		System.out.print("\n----------------------------------");
+		System.out.print("\n-------------------");
 		
 			for (int i = 0; i < sides.size(); i++) 
 			{
-				System.out.printf("\n\n%-1s)%-22s|£%-19.2f", i+1, sides.get(i), sidesPrice.get(i));//Only our sides will be printed out here,even without the price. The meal has a set price and they can choose whatever side the customer wants
+				System.out.printf("\n\n%-1s)%-22s", i+1, sides.get(i));//Only our sides will be printed out here,even without the price. The meal has a set price and they can choose whatever side the customer wants
 				//System.out.printf("\n----------------------------------------------");
 			}
 			
-		option = MyVal.validIntRange("\n\nPlease choose an option above", 1, sides.size());
+		int sideOption = MyVal.validIntRange("\n\nPlease choose an option above", 1, sides.size());
 		
-		System.out.println("\n" + sides.get(option-1) + "\nAdded as a side.");
+		System.out.println("\n" + sides.get(sideOption-1) + "\nAdded as a side.");
 		
-		int quantity = quantity();
+		quantity = quantity();
+		
+		System.out.print(mealPrice.toString());
 		
 		qtyTotal(quantity, mealPrice.get(option-1));
 		
 		notes.add("+ " + sides.get(option-1) + " side");
-		
-		priceBasket.add(mealPrice.get(foodItem));
 		
 		yesNo = MyVal.validYN("\nReturn to hot food selection (Y), or home (N)?");
 		
@@ -238,10 +296,26 @@ public class Ex1 {
 			}
 	}
 	
+	static void addNotes ()
+	{
+		yesNo = MyVal.validYN("Are there any notes or further requirements for this item?");
+		if(yesNo == 'N')
+		{
+			notes.add("");
+		}
+			else
+			{
+				My.p("Note : ");
+				Scanner key = new Scanner (System.in);
+				note = key.nextLine();
+				notes.add(note);
+			}
+	}
+	
 	static void goHome()
 
 	{
-		yesNo = MyVal.validYN("\nReturn to hot food selection (Y), or home (N)?");
+		yesNo = MyVal.validYN("\nReturn to same food selection (Y), or home (N)?");
 		
 		if (yesNo == 'N')
 		{
@@ -260,7 +334,6 @@ public class Ex1 {
 		return Ex1.printFoodOption(option);
 	}
 	
-	
 	static void mainMenu()
 	{
 
@@ -268,11 +341,11 @@ public class Ex1 {
 		
 		switch (option)
 		{
-		/*case 1:
+		case 1:
 	       	System.out.println("");
 	        break;
-		case 2:
-	       	System.out.println("");
+		/*case 2:
+	       	System.out.println("No work from nathan");
 	        break;*/
 		case 3:
 			hotFood();
@@ -327,7 +400,6 @@ public class Ex1 {
 	{
 		String staff_1 = "jmcelwee";
 		String staff_2 = "treety";
-		String staff_3 = "nmorrow";
 		String login = "";
 		
 		Scanner key = new Scanner (System.in);
@@ -336,7 +408,7 @@ public class Ex1 {
 		
 		int count = 0;
 		
-		if (login.equals(staff_1)| login.equals(staff_2)| login.equals(staff_3))
+		if (login.equals(staff_1)| login.equals(staff_2))
 		{
 			My.pl("\nWelcome Staff member. Remeber you get an automatic 10% discount on your order");
 			mainMenu();
