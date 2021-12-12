7738d8b0493b0b7845c55fe4389f75453323f435
Jordi9047 <95035711+Jordi9047@users.noreply.github.com>
2021-12-12 23:15:52 +0000

    Add files via upload

---
+++ general/My.java
@@ -0,0 +1,19 @@
+package general;
+
+public class My 
+{
+	public static void p(String s)
+	{
+		System.out.print(s);
+	}
+	
+	public static void pl(String s)
+	{
+		System.out.println(s);
+	}
+	
+	public static void pf(String s)
+	{
+		System.out.printf(s);
+	}
+}
